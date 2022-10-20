8e1b1cb53f641b8bce224aa8a2cc567215708123
na1307 <na1307@outlook.kr>
2022-10-20 12:55:56 +0900

    Remove dependencies

---
+++ Bluehill.Hangul/ExtensionAttribute.cs
@@ -0,0 +1,6 @@
+﻿#if NETFRAMEWORK && !NET35_OR_GREATER
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+internal sealed class ExtensionAttribute : Attribute { }
+#endif
