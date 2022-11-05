3b3ce93cca037d1dcf15f18efff8040c021da8b2
na1307 <na1307@outlook.kr>
2022-11-05 12:11:19 +0900

    테스트 변경

---
+++ Bluehill.Hangul.Tests/CharExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿namespace Bluehill.Hangul.Tests;
+
+public sealed class CharExtensionsTest {
+    const char han = '한';
+    const char giyeok = 'ㄱ';
+    const char s = 's';
+
+    [Fact]
+    public void IsHangulTest() {
+        Assert.True(han.IsHangul());
+        Assert.False(giyeok.IsHangul());
+        Assert.False(s.IsHangul());
+    }
+
+    [Fact]
+    public void IsJamoTest() {
+        Assert.False(han.IsJamo());
+        Assert.True(giyeok.IsJamo());
+        Assert.False(s.IsJamo());
+    }
+
+    [Fact]
+    public void ChoseongTest() {
+        Assert.Equal(Choseong.Hieut, han.Choseong());
+        Assert.Throws<ArgumentException>(() => giyeok.Choseong());
+        Assert.Throws<ArgumentException>(() => s.Choseong());
+    }
+
+    [Fact]
+    public void JungseongTest() {
+        Assert.Equal(Jungseong.A, han.Jungseong());
+        Assert.Throws<ArgumentException>(() => giyeok.Jungseong());
+        Assert.Throws<ArgumentException>(() => s.Jungseong());
+    }
+
+    [Fact]
+    public void JongseongTest() {
+        Assert.Equal(Jongseong.Nieun, han.Jongseong());
+        Assert.Throws<ArgumentException>(() => giyeok.Jungseong());
+        Assert.Throws<ArgumentException>(() => s.Jungseong());
+    }
+}
---
+++ Bluehill.Hangul.Tests/EnumExtensionsTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Bluehill.Hangul.Tests;
+
+public sealed class EnumExtensionsTest {
+    [Fact]
+    public void ToCharTest() {
+        Assert.Equal('ㅇ', Choseong.Ieung.ToChar());
+        Assert.Equal('ㅏ', Jungseong.A.ToChar());
+        Assert.Equal('ㄴ', Jongseong.Nieun.ToChar());
+    }
+}
