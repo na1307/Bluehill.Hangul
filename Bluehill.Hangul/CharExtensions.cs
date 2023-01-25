9dc88d5e54a5c0b4f4f6039d4c8d38e370fd29a0
na1307 <na1307@outlook.kr>
2023-01-25 10:05:52 +0900

    표기 수정

--- Bluehill.Hangul/CharExtensions.cs
+++ Bluehill.Hangul/CharExtensions.cs
@@ -5,10 +5,10 @@
 /// </summary>
 public static class CharExtensions {
     /// <summary>
-    /// 문자가 한글 문자인지 여부
+    /// 문자가 한글 음절인지 여부
     /// </summary>
     /// <param name="c">문자</param>
-    /// <returns>문자가 한글 문자라면 <see langword="true"/>, 아니라면 <see langword="false"/></returns>
+    /// <returns>문자가 한글 음절이라면 <see langword="true"/>, 아니라면 <see langword="false"/></returns>
     public static bool IsHangul(this char c) => c is >= FirstChar and <= LastChar;
 
     /// <summary>
@@ -19,26 +19,26 @@ public static class CharExtensions {
     public static bool IsJamo(this char c) => c is >= FirstJamo and <= LastJamo;
 
     /// <summary>
-    /// 이 한글 문자의 <see cref="Hangul.Choseong"/> 값을 반환함
+    /// 이 한글 음절의 초성에 해당하는 <see cref="Hangul.Choseong"/> 값을 반환함
     /// </summary>
-    /// <param name="hangulChar">한글 문자</param>
-    /// <returns><paramref name="hangulChar"/>의 초성에 대한 <see cref="Hangul.Choseong"/> 값</returns>
-    /// <exception cref="ArgumentException"><paramref name="hangulChar"/>가 한글 문자가 아님</exception>
-    public static Choseong Choseong(this char hangulChar) => hangulChar.IsHangul() ? (Choseong)((hangulChar - FirstChar) / 28 / 21) : throw new ArgumentException("문자가 한글 문자가 아닙니다.");
+    /// <param name="hangulSyllable">한글 음절</param>
+    /// <returns><paramref name="hangulSyllable"/>의 초성에 해당하는 <see cref="Hangul.Choseong"/> 값</returns>
+    /// <exception cref="ArgumentException"><paramref name="hangulSyllable"/>가 한글 음절이 아님</exception>
+    public static Choseong Choseong(this char hangulSyllable) => hangulSyllable.IsHangul() ? (Choseong)((hangulSyllable - FirstChar) / 28 / 21) : throw new ArgumentException("문자가 한글 음절 문자가 아닙니다.");
 
     /// <summary>
-    /// 이 한글 문자의 <see cref="Hangul.Jungseong"/> 값을 반환함
+    /// 이 한글 음절의 중성에 해당하는 <see cref="Hangul.Jungseong"/> 값을 반환함
     /// </summary>
-    /// <param name="hangulChar">한글 문자</param>
-    /// <returns><paramref name="hangulChar"/>의 초성에 대한 <see cref="Hangul.Jungseong"/> 값</returns>
-    /// <exception cref="ArgumentException"><paramref name="hangulChar"/>가 한글 문자가 아님</exception>
-    public static Jungseong Jungseong(this char hangulChar) => hangulChar.IsHangul() ? (Jungseong)((hangulChar - FirstChar) / 28 % 21) : throw new ArgumentException("문자가 한글 문자가 아닙니다.");
+    /// <param name="hangulSyllable">한글 음절</param>
+    /// <returns><paramref name="hangulSyllable"/>의 초성에 해당하는 <see cref="Hangul.Jungseong"/> 값</returns>
+    /// <exception cref="ArgumentException"><paramref name="hangulSyllable"/>가 한글 음절이 아님</exception>
+    public static Jungseong Jungseong(this char hangulSyllable) => hangulSyllable.IsHangul() ? (Jungseong)((hangulSyllable - FirstChar) / 28 % 21) : throw new ArgumentException("문자가 한글 음절 문자가 아닙니다.");
 
     /// <summary>
-    /// 이 한글 문자의 <see cref="Hangul.Jongseong"/> 값을 반환함
+    /// 이 한글 음절의 종성에 해당하는 <see cref="Hangul.Jongseong"/> 값을 반환함
     /// </summary>
-    /// <param name="hangulChar">한글 문자</param>
-    /// <returns><paramref name="hangulChar"/>의 초성에 대한 <see cref="Hangul.Jongseong"/> 값</returns>
-    /// <exception cref="ArgumentException"><paramref name="hangulChar"/>가 한글 문자가 아님</exception>
-    public static Jongseong Jongseong(this char hangulChar) => hangulChar.IsHangul() ? (Jongseong)((hangulChar - FirstChar) % 28) : throw new ArgumentException("문자가 한글 문자가 아닙니다.");
+    /// <param name="hangulSyllable">한글 음절</param>
+    /// <returns><paramref name="hangulSyllable"/>의 초성에 해당하는 <see cref="Hangul.Jongseong"/> 값</returns>
+    /// <exception cref="ArgumentException"><paramref name="hangulSyllable"/>가 한글 음절이 아님</exception>
+    public static Jongseong Jongseong(this char hangulSyllable) => hangulSyllable.IsHangul() ? (Jongseong)((hangulSyllable - FirstChar) % 28) : throw new ArgumentException("문자가 한글 음절 문자가 아닙니다.");
 }
