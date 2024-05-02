fe6069ed1461074ffb37c6f4c547714e3c81e11f
Jonghyo Lee <na1307@outlook.kr>
2024-05-02 16:25:39 +0900

    새로운 GitHub Pages

---
+++ Bluehill.Hangul.Pages/Program.cs
@@ -0,0 +1,33 @@
+using Bluehill.Hangul.Pages;
+using HighlightBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddLocalization();
+
+builder.Services.AddHighlight();
+
+var host = builder.Build();
+
+const string defaultCulture = "ko-KR";
+
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
+
+if (result == null) {
+    await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
