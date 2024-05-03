2633c440abdf40255e3b116d010b19f8ed74861d
Jonghyo Lee <na1307@outlook.kr>
2024-05-03 12:52:28 +0900

    Fix Pages and Tests

---
+++ Bluehill.Hangul.Pages.Tests/TryCharTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Bluehill.Hangul.Pages.Tests;
+
+public sealed class TryCharTest : TestContext {
+    public TryCharTest() {
+        Services.AddLocalization();
+    }
+
+    [Fact]
+    public void TryChar_Results_Empty() {
+        var localizer = Services.GetService<IStringLocalizer<TryChar>>()!;
+
+        // Act
+        var cut = RenderComponent<TryChar>();
+
+        // Assert
+        cut.Find("table#results").MarkupMatches($"""
+            <table id="results" class="table">
+              <thead>
+                <tr>
+                  <th>{localizer.GetString("Char")}</th>
+                  <th>{localizer.GetString("Choseong")}</th>
+                  <th>{localizer.GetString("Jungseong")}</th>
+                  <th>{localizer.GetString("Jongseong")}</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+            """);
+    }
+
+    [Theory]
+    [InlineData("나는 사과를 먹는다.")]
+    [InlineData("Word bird wondering i desolate this then flutter and tufted sure i memories more thereis memories felt saintly land as")]
+    public void TryChar_Results_Inputs(string input) {
+        var localizer = Services.GetService<IStringLocalizer<TryChar>>()!;
+
+        // Act
+        var cut = RenderComponent<TryChar>();
+
+        cut.Find("input#input").Change(input);
+
+        // Assert
+        cut.Find("table#results").MarkupMatches($"""
+            <table id="results" class="table">
+              <thead>
+                <tr>
+                  <th>{localizer.GetString("Char")}</th>
+                  <th>{localizer.GetString("Choseong")}</th>
+                  <th>{localizer.GetString("Jungseong")}</th>
+                  <th>{localizer.GetString("Jongseong")}</th>
+                </tr>
+              </thead>
+              <tbody>
+            """ + getOutput() + """
+              </tbody>
+            </table>
+            """);
+
+        string getOutput() {
+            StringBuilder builder = new();
+
+            foreach (var c in input) {
+                builder.Append($"""
+                <tr>
+                    <td>{c}</td>
+                """);
+
+                if (c.IsHangulSyllable()) {
+                    builder.Append($"""
+                        <td>{$"{c.Choseong().ToJamo()} ({c.Choseong()})"} </td>
+                        <td>{$"{c.Jungseong().ToJamo()} ({c.Jungseong()})"} </td>
+                        <td>{(c.Jongseong() != Jongseong.None ? $"{c.Jongseong().ToJamo()} ({c.Jongseong()})" : $"{localizer.GetString("NoneJongseong")} ({Jongseong.None})")}</td>
+                    """);
+                } else {
+                    builder.Append($"""
+                        <td colspan="3" style="text-align: center;">{localizer.GetString("NotHangulSyllable")}</td>
+                    """);
+                }
+
+                builder.Append("""
+                </tr>
+                """);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Bluehill.Hangul.Pages.Tests/TryJosaTest.cs
@@ -0,0 +1,114 @@
+﻿namespace Bluehill.Hangul.Pages.Tests;
+
+public sealed class TryJosaTest : TestContext {
+    public TryJosaTest() {
+        Services.AddLocalization();
+    }
+
+    [Fact]
+    public void TryJosa_Results_Empty() {
+        var localizer = Services.GetService<IStringLocalizer<TryJosa>>()!;
+
+        // Act
+        var cut = RenderComponent<TryJosa>();
+
+        // Assert
+        cut.Find("table#results").MarkupMatches($"""
+            <table id="results" class="table">
+              <thead>
+                <tr>
+                  <th>{localizer.GetString("Method")}</th>
+                  <th>{localizer.GetString("Result")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>EunNeun</td>
+                  <td>은(는)</td>
+                </tr>
+                <tr>
+                  <td>IGa</td>
+                  <td>이(가)</td>
+                </tr>
+                <tr>
+                  <td>I</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>EulReul</td>
+                  <td>을(를)</td>
+                </tr>
+                <tr>
+                  <td>GwaWa</td>
+                  <td>과(와)</td>
+                </tr>
+                <tr>
+                  <td>AYa</td>
+                  <td>아(야)</td>
+                </tr>
+                <tr>
+                  <td>EuRo</td>
+                  <td>(으)로</td>
+                </tr>
+              </tbody>
+            </table>
+            """);
+    }
+
+    [Theory]
+    [InlineData("사과")]
+    [InlineData("하준")]
+    [InlineData("은율")]
+    [InlineData("Ben")]
+    [InlineData("ㅏ")]
+    public void TryJosa_Results_Inputs(string input) {
+        var localizer = Services.GetService<IStringLocalizer<TryJosa>>()!;
+
+        // Act
+        var cut = RenderComponent<TryJosa>();
+
+        cut.Find("input#input").Change(input);
+
+        // Assert
+        cut.Find("table#results").MarkupMatches($"""
+            <table id="results" class="table">
+              <thead>
+                <tr>
+                  <th>{localizer.GetString("Method")}</th>
+                  <th>{localizer.GetString("Result")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>EunNeun</td>
+                  <td>{input.EunNeun()}</td>
+                </tr>
+                <tr>
+                  <td>IGa</td>
+                  <td>{input.IGa()}</td>
+                </tr>
+                <tr>
+                  <td>I</td>
+                  <td>{input.I()}</td>
+                </tr>
+                <tr>
+                  <td>EulReul</td>
+                  <td>{input.EulReul()}</td>
+                </tr>
+                <tr>
+                  <td>GwaWa</td>
+                  <td>{input.GwaWa()}</td>
+                </tr>
+                <tr>
+                  <td>AYa</td>
+                  <td>{input.AYa()}</td>
+                </tr>
+                <tr>
+                  <td>EuRo</td>
+                  <td>{input.EuRo()}</td>
+                </tr>
+              </tbody>
+            </table>
+            """);
+    }
+}
