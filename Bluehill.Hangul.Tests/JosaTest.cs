7a486bce3e041b3f8a83cf498c80681c8defdd01
Jonghyo Lee <na1307@outlook.kr>
2024-05-14 17:31:52 +0900

    JosaTest 재작성

--- Bluehill.Hangul.Tests/JosaTest.cs
+++ Bluehill.Hangul.Tests/JosaTest.cs
@@ -4,245 +4,244 @@ public sealed class JosaTest {
     private const string sagwa = "사과";
     private const string subak = "수박";
     private const string gamgyul = "감귤";
+    private const string banana = "Banana";
     private const string apple = "Apple";
     private const string watermellon = "Watermellon";
     private const string yunha = "윤하";
     private const string hajun = "하준";
     private const string eunyul = "은율";
-    private const string michael = "Michael";
+    private const string julie = "Julie";
     private const string ben = "Ben";
+    private const string michael = "Michael";
     private const string daegu = "대구";
     private const string busan = "부산";
     private const string seoul = "서울";
     private const string california = "California";
     private const string newyork = "New York";
 
-    [Fact]
-    public void EunNeunTest() {
-        // Expressions
-        var r1 = sagwa.EunNeun();
-        var r2 = subak.EunNeun();
-        var r3 = gamgyul.EunNeun();
-        var r4 = apple.EunNeun();
-        var r5 = watermellon.EunNeun(Eun);
-        var r6 = sagwa.EunNeun(Eul_Reul, true);
+    [Theory]
+    [InlineData(sagwa, Neun)]
+    [InlineData(subak, Eun)]
+    [InlineData(gamgyul, Eun)]
+    [InlineData(banana, Eun_Neun)]
+    [InlineData(apple, Eun_Neun)]
+    [InlineData(watermellon, Eun_Neun)]
+    public void EunNeun_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EunNeun());
+    }
 
-        // Expected
-        const string e1 = sagwa + Neun;
-        const string e2 = subak + Eun;
-        const string e3 = gamgyul + Eun;
-        const string e4 = apple + Eun_Neun;
-        const string e5 = watermellon + Eun;
-        const string e6 = Neun;
+    [Theory]
+    [InlineData(banana, Neun)]
+    [InlineData(apple, Eun)]
+    [InlineData(watermellon, Eun)]
+    public void EunNeun_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EunNeun(expectedJosa));
+    }
 
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    [Theory]
+    [InlineData(banana, Neun)]
+    [InlineData(apple, Eun)]
+    [InlineData(watermellon, Eun)]
+    public void EunNeun_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.EunNeun(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(yunha, Ga)]
+    [InlineData(hajun, I)]
+    [InlineData(eunyul, I)]
+    [InlineData(julie, I_Ga)]
+    [InlineData(michael, I_Ga)]
+    [InlineData(ben, I_Ga)]
+    public void IGa_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.IGa());
+    }
+
+    [Theory]
+    [InlineData(julie, Ga)]
+    [InlineData(michael, I)]
+    [InlineData(ben, I)]
+    public void IGa_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.IGa(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(julie, Ga)]
+    [InlineData(michael, I)]
+    [InlineData(ben, I)]
+    public void IGa_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.IGa(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(yunha, "")]
+    [InlineData(hajun, I)]
+    [InlineData(eunyul, I)]
+    [InlineData(julie, "")]
+    [InlineData(michael, "")]
+    [InlineData(ben, "")]
+    public void I_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.I());
+    }
+
+    [Theory]
+    [InlineData(julie, "")]
+    [InlineData(michael, I)]
+    [InlineData(ben, I)]
+    public void I_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.I(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(julie, "")]
+    [InlineData(michael, I)]
+    [InlineData(ben, I)]
+    public void I_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.I(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(sagwa, Reul)]
+    [InlineData(subak, Eul)]
+    [InlineData(gamgyul, Eul)]
+    [InlineData(banana, Eul_Reul)]
+    [InlineData(apple, Eul_Reul)]
+    [InlineData(watermellon, Eul_Reul)]
+    public void EulReul_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EulReul());
+    }
+
+    [Theory]
+    [InlineData(banana, Reul)]
+    [InlineData(apple, Eul)]
+    [InlineData(watermellon, Eul)]
+    public void EulReul_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EulReul(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(banana, Reul)]
+    [InlineData(apple, Eul)]
+    [InlineData(watermellon, Eul)]
+    public void EulReul_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.EulReul(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(yunha, Wa)]
+    [InlineData(hajun, Gwa)]
+    [InlineData(eunyul, Gwa)]
+    [InlineData(julie, Gwa_Wa)]
+    [InlineData(michael, Gwa_Wa)]
+    [InlineData(ben, Gwa_Wa)]
+    public void GwaWa_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.GwaWa());
+    }
+
+    [Theory]
+    [InlineData(julie, Wa)]
+    [InlineData(michael, Gwa)]
+    [InlineData(ben, Gwa)]
+    public void GwaWa_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.GwaWa(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(julie, Wa)]
+    [InlineData(michael, Gwa)]
+    [InlineData(ben, Gwa)]
+    public void GwaWa_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.GwaWa(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(yunha, Ya)]
+    [InlineData(hajun, A)]
+    [InlineData(eunyul, A)]
+    [InlineData(julie, A_Ya)]
+    [InlineData(michael, A_Ya)]
+    [InlineData(ben, A_Ya)]
+    public void AYa_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.AYa());
+    }
+
+    [Theory]
+    [InlineData(julie, Ya)]
+    [InlineData(michael, A)]
+    [InlineData(ben, A)]
+    public void AYa_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.AYa(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(julie, Ya)]
+    [InlineData(michael, A)]
+    [InlineData(ben, A)]
+    public void AYa_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.AYa(expectedJosa, true));
+    }
+
+    [Theory]
+    [InlineData(daegu, Ro)]
+    [InlineData(busan, Euro)]
+    [InlineData(seoul, Ro)]
+    [InlineData(california, Eu_Ro)]
+    [InlineData(newyork, Eu_Ro)]
+    public void EuRo_Test(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EuRo());
+    }
+
+    [Theory]
+    [InlineData(california, Ro)]
+    [InlineData(newyork, Euro)]
+    public void EuRo_Test_DefaultJosa(string str, string expectedJosa) {
+        Assert.Equal(str + expectedJosa, str.EuRo(expectedJosa));
+    }
+
+    [Theory]
+    [InlineData(california, Ro)]
+    [InlineData(newyork, Euro)]
+    public void EuRo_Test_DefaultJosa_JosaOnly(string str, string expectedJosa) {
+        Assert.Equal(expectedJosa, str.EuRo(expectedJosa, true));
     }
 
     [Fact]
-    public void IGaTest() {
-        // Expressions
-        var r1 = yunha.IGa();
-        var r2 = hajun.IGa();
-        var r3 = eunyul.IGa();
-        var r4 = michael.IGa();
-        var r5 = ben.IGa(I);
-        var r6 = yunha.IGa(I_Ga, true);
-
-        // Expected
-        const string e1 = yunha + Ga;
-        const string e2 = hajun + I;
-        const string e3 = eunyul + I;
-        const string e4 = michael + I_Ga;
-        const string e5 = ben + I;
-        const string e6 = Ga;
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void Jongseong_StrIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.Jongseong(null!, string.Empty, string.Empty, string.Empty, false));
     }
 
     [Fact]
-    public void ITest() {
-        // Expressions
-        var r1 = yunha.I();
-        var r2 = hajun.I();
-        var r3 = eunyul.I();
-        var r4 = michael.I();
-        var r5 = ben.I(I);
-        var r6 = yunha.I(string.Empty, true);
-
-        // Expected
-        const string e1 = yunha;
-        const string e2 = hajun + I;
-        const string e3 = eunyul + I;
-        const string e4 = michael;
-        const string e5 = ben + I;
-        const string e6 = "";
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void Jongseong_DefaultJosaIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.Jongseong(string.Empty, null!, string.Empty, string.Empty, false));
     }
 
     [Fact]
-    public void EulReulTest() {
-        // Expressions
-        var r1 = sagwa.EulReul();
-        var r2 = subak.EulReul();
-        var r3 = gamgyul.EulReul();
-        var r4 = apple.EulReul();
-        var r5 = watermellon.EulReul(Eul);
-        var r6 = sagwa.EulReul(Eul_Reul, true);
-
-        // Expected
-        const string e1 = sagwa + Reul;
-        const string e2 = subak + Eul;
-        const string e3 = gamgyul + Eul;
-        const string e4 = apple + Eul_Reul;
-        const string e5 = watermellon + Eul;
-        const string e6 = Reul;
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void Jongseong_JongseongIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.Jongseong(string.Empty, string.Empty, null!, string.Empty, false));
     }
 
     [Fact]
-    public void GwaWaTest() {
-        // Expressions
-        var r1 = yunha.GwaWa();
-        var r2 = hajun.GwaWa();
-        var r3 = eunyul.GwaWa();
-        var r4 = michael.GwaWa();
-        var r5 = ben.GwaWa(Gwa);
-        var r6 = yunha.GwaWa(Gwa_Wa, true);
-
-        // Expected
-        const string e1 = yunha + Wa;
-        const string e2 = hajun + Gwa;
-        const string e3 = eunyul + Gwa;
-        const string e4 = michael + Gwa_Wa;
-        const string e5 = ben + Gwa;
-        const string e6 = Wa;
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void Jongseong_NoJongseongIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.Jongseong(string.Empty, string.Empty, string.Empty, null!, false));
     }
 
     [Fact]
-    public void AYaTest() {
-        // Expressions
-        var r1 = yunha.AYa();
-        var r2 = hajun.AYa();
-        var r3 = eunyul.AYa();
-        var r4 = michael.AYa();
-        var r5 = ben.AYa(A);
-        var r6 = yunha.AYa(A_Ya, true);
-
-        // Expected
-        const string e1 = yunha + Ya;
-        const string e2 = hajun + A;
-        const string e3 = eunyul + A;
-        const string e4 = michael + A_Ya;
-        const string e5 = ben + A;
-        const string e6 = Ya;
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void NoJongseongOrRieul_StrIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.NoJongseongOrRieul(null!, string.Empty, string.Empty, string.Empty, false));
     }
 
     [Fact]
-    public void EuRoTest() {
-        // Expressions
-        var r1 = daegu.EuRo();
-        var r2 = busan.EuRo();
-        var r3 = seoul.EuRo();
-        var r4 = california.EuRo();
-        var r5 = newyork.EuRo(Euro);
-        var r6 = daegu.EuRo(Eu_Ro, true);
-
-        // Expected
-        const string e1 = daegu + Ro;
-        const string e2 = busan + Euro;
-        const string e3 = seoul + Ro;
-        const string e4 = california + Eu_Ro;
-        const string e5 = newyork + Euro;
-        const string e6 = Ro;
-
-        // Asserts
-        Assert.Equal(e1, r1);
-        Assert.Equal(e2, r2);
-        Assert.Equal(e3, r3);
-        Assert.Equal(e4, r4);
-        Assert.Equal(e5, r5);
-        Assert.Equal(e6, r6);
+    public void NoJongseongOrRieul_DefaultJosaIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.NoJongseongOrRieul(string.Empty, null!, string.Empty, string.Empty, false));
     }
 
     [Fact]
-    public void Jongseong_ArgumentNullException1() {
-        Assert.Throws<ArgumentNullException>(() => ((string)null!).Jongseong(string.Empty, string.Empty, string.Empty, false));
+    public void NoJongseongOrRieul_RieulIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.NoJongseongOrRieul(string.Empty, string.Empty, null!, string.Empty, false));
     }
 
     [Fact]
-    public void Jongseong_ArgumentNullException2() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.Jongseong(null!, string.Empty, string.Empty, false));
-    }
-
-    [Fact]
-    public void Jongseong_ArgumentNullException3() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.Jongseong(string.Empty, null!, string.Empty, false));
-    }
-
-    [Fact]
-    public void Jongseong_ArgumentNullException4() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.Jongseong(string.Empty, string.Empty, null!, false));
-    }
-
-    [Fact]
-    public void NoJongseongOrRieul_ArgumentNullException1() {
-        Assert.Throws<ArgumentNullException>(() => ((string)null!).NoJongseongOrRieul(string.Empty, string.Empty, string.Empty, false));
-    }
-
-    [Fact]
-    public void NoJongseongOrRieul_ArgumentNullException2() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.NoJongseongOrRieul(null!, string.Empty, string.Empty, false));
-    }
-
-    [Fact]
-    public void NoJongseongOrRieul_ArgumentNullException3() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.NoJongseongOrRieul(string.Empty, null!, string.Empty, false));
-    }
-
-    [Fact]
-    public void NoJongseongOrRieul_ArgumentNullException4() {
-        Assert.Throws<ArgumentNullException>(() => sagwa.NoJongseongOrRieul(string.Empty, string.Empty, null!, false));
+    public void NoJongseongOrRieul_NoRieulIsNull_ThrowsArgumentNullException() {
+        Assert.Throws<ArgumentNullException>(() => Josa.NoJongseongOrRieul(string.Empty, string.Empty, string.Empty, null!, false));
     }
 }
