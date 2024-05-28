6da5aa78835ad734ca7a3eb561ff81e204ef6f76
Jonghyo Lee <na1307@outlook.kr>
2024-05-28 15:02:40 +0900

    Pages

--- Bluehill.Hangul.Pages/Program.cs
+++ Bluehill.Hangul.Pages/Program.cs
@@ -1,39 +1,22 @@
+using Bluehill.Blazor;
+using Bluehill.Hangul.Pages;
 using HighlightBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.JSInterop;
-using System.Globalization;
 
-namespace Bluehill.Hangul.Pages;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddLocalization();
+
+builder.Services.AddHighlight();
+
+var host = await builder.BuildAndSetCultureAsync("ko-KR");
+
+await host.RunAsync();
 
 [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-internal static class Program {
-    private static async Task Main(string[] args) {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
-
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-        builder.Services.AddLocalization();
-
-        builder.Services.AddHighlight();
-
-        var host = builder.Build();
-
-        const string defaultCulture = "ko-KR";
-
-        var js = host.Services.GetRequiredService<IJSRuntime>();
-        var result = await js.InvokeAsync<string>("blazorCulture.get");
-        var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
-
-        if (result == null) {
-            await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
-        }
-
-        CultureInfo.DefaultThreadCurrentCulture = culture;
-        CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-        await host.RunAsync();
-    }
-}
+internal static partial class Program;
